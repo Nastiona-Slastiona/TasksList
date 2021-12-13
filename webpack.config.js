e3a2c0855a5b7f52521c4e3d77054d6abc8041c3
Anastasiya Kruglaya <ariaignis.dragon@gmail.com>
2021-12-13 13:37:25 +0300

    Added webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+  },
+  devServer: {
+      port: 3000,
+      hot: true
+  },
+  plugins: [
+      new HTMLWebpackPlugin({ template: "./public/index.html"}),
+      new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env' , '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ]
+  }
+};
